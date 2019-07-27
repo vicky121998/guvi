1090396ca1b03cd462bdbd4b5e687ec5566fab97
vicky121998 <36404884+vicky121998@users.noreply.github.com>
2019-07-27 14:57:28 +0530

    Create stringn.java

---
+++ stringn.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+        int v=sc.nextInt();
+        String s=sc.nextLine();
+        for(int i=0;i<v;i++)
+        {
+        	System.out.println(s+"\n");
+        }
+  
+	}
+}
