81598138b0ec56c5acd3b4306e5eed69be1eda0c
vicky121998 <36404884+vicky121998@users.noreply.github.com>
2019-07-27 15:04:20 +0530

    Create intrange.java

---
+++ intrange.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+        int v=sc.nextInt();
+        if((v>=1)&&(v<=10))
+      
+        	System.out.println("yes");
+      else
+      System.out.println("no");
+  
+	}
+}
